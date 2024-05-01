f901750f600305dbc03d367b544a5dcbda5fa57f
Paulo Cerqueira <digelopaulo@hotmail.com>
2024-05-01 11:56:23 -0300

    comitando

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from flask import flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+"""
 from flask import Flask
 from views import configure_routes
 
@@ -9,3 +18,4 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
+"""
