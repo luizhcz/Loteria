77081ee98bb04937af6d0955f94b5f2f383e7717
Paulo Cerqueira <digelopaulo@hotmail.com>
2024-05-01 01:49:14 -0300

    sobe

--- Loteria.App/Loteria.App/Saida.cs
+++
@@ -1,9 +0,0 @@
-namespace Loteria.App
-{
-    public class Saida
-    {
-        public int totalCombinacoes { get; set; }
-        public List<int[]> Combinacoes { get; set; }
-        public int[] NumeroGerados { get; set; }
-    }
-}
